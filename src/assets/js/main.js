f1dab10d0876bfbaa36cf71fbaf72fa560e589bd
grevtsovweb <grevtsov.web@gmail.com>
2021-02-26 10:43:49 +0300

    Addition v2.0

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,27 +3,39 @@ document.addEventListener('DOMContentLoaded', function(){
     let offer = document.querySelector('.page__offer');
     let calendar = document.querySelector('.page__calendar');
     let socialNav = document.querySelector('.page__social-nav');
+    let progressBlock = document.querySelector('.page__progress-js');
 
     let replaceBlockToSidebar = function(elem){
         if(window.innerWidth > 760 && elem){
             let sidebar = document.querySelector('.page__sidebar');
             
-
-            sidebar.append(elem);
+            if(sidebar){
+                sidebar.append(elem);
+            }  
         }
     }
 
     let replaceCalendarDesktop = function(elem){
         if(window.innerWidth > 992 && elem){
-            let container = document.querySelector('.page__grid');
+            let container = document.querySelector('.page__content-row-js');
             
 
             container.append(elem);
         }
     }
 
+    let replaceProgressBlock = function(elem){
+        if(window.innerWidth > 767 && elem){
+            let container = document.querySelector('.page__grid-js');
+            
+
+            container.prepend(elem);
+        }
+    }
+
     replaceBlockToSidebar(offer);
     replaceBlockToSidebar(calendar);
     replaceBlockToSidebar(socialNav);
-    replaceCalendarDesktop(calendar)
+    replaceCalendarDesktop(calendar);
+    replaceProgressBlock(progressBlock);
 });
